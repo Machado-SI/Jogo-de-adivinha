e670f14a554191109064b59c24860fcabd91f50f
Machado-SI <223562434+Machado-SI@users.noreply.github.com>
2025-08-01 19:36:26 -0300

    Correção do arquivo index.js e adição de package-lock.json

--- index.js
+++ index.js
@@ -9,23 +9,52 @@ const rl = readline.createInterface({
 
 let numeroSecreto
 let tentativas = 0
-const MIN_TENTATIVAS = 10
+const MAX_TENTATIVAS = 10
 const MIN_NUMERO = 1
-const MAX_NUMMERO = 100
+const MAX_NUMERO = 100
 
 function InicialJogo() {
     //gera um número aleatório
-    const numeroAleatorio = Math.floor(Math.random() * (MAX_NUMMERO - MIN_NUMERO + 1)) + MIN_NUMERO
+    numeroSecreto = Math.floor(Math.random() * (MAX_NUMERO - MIN_NUMERO + 1)) + MIN_NUMERO
     tentativas = 0
     console.log(
         `Olá bem vindo ao jogo de adivinhar número!
          Tente adivinhar o número secreto entre ${MIN_NUMERO} e ${MIN_NUMERO}
-         Você tem ${tentativas}. Boa sorte
+         Você tem ${tentativas} tentativas. Boa sorte
         `
     )
     pedirPalpite()
 }
 
 function pedirPalpite() {
+    if(tentativas >= MAX_TENTATIVAS) {
+        console.log(`\nFim de jogo! Você excedeu o limite de ${MAX_TENTATIVAS} tentativas.`)
+        console.log(`O número secreto era ${numeroSecreto}.`)
+        rl.close()
+        return
+    }
 
+    rl.question('Qual é seu palpite: ', (resposta) => {
+        const palpite = parseInt(resposta)
+        tentativas++
+
+        if(isNaN(palpite)) {
+        console.log('Digite um número válido')
+        pedirPalpite()
+        return
+    }
+
+    if(palpite === numeroSecreto) {
+        console.log(`\nParábens! Você acertou o número secreto (${numeroSecreto}) em ${tentativas} tentativas.`)
+        rl.close()
+    } else if (palpite < numeroSecreto) {
+        console.log(`O número secreto é MAIOR que ${palpite}. Tentativas: ${tentativas}`)
+        pedirPalpite()
+    } else if (palpite > numeroSecreto) {
+        console.log(`Número secreto é MENOR que ${palpite}. Tentativas: ${tentativas}`)
+        pedirPalpite()
+    }
+    })
 }
+
+InicialJogo()
